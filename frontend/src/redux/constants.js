d7a06e598aab04aa2166da75fa25b7067303e823
Minh20812 <blossworld12@gmail.com>
2024-10-24 23:20:01 +0700

    change13

--- frontend/src/redux/constants.js
+++ frontend/src/redux/constants.js
@@ -1,5 +1,9 @@
-export const BASE_URL = "";
-export const USERS_URL = "/api/users";
-export const TASKS_URL = "/api/tasks";
-export const PROJECTS_URL = "/api/projects";
-export const LABELS_URL = "/api/labels";
+export const BASE_URL =
+  process.env.NODE_ENV === "production"
+    ? "https://todolist-app-d0wu.onrender.com/"
+    : "";
+
+export const USERS_URL = `${BASE_URL}/api/users`;
+export const TASKS_URL = `${BASE_URL}/api/tasks`;
+export const PROJECTS_URL = `${BASE_URL}/api/projects`;
+export const LABELS_URL = `${BASE_URL}/api/labels`;
