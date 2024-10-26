6b6831c11a847bdea14e7cc1b826ab2df7b49a0d
Minh20812 <blossworld12@gmail.com>
2024-10-26 10:42:48 +0700

    change15

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -1,17 +1,3 @@
-// import { defineConfig } from "vite";
-// import react from "@vitejs/plugin-react";
-
-// // https://vitejs.dev/config/
-// export default defineConfig({
-//   plugins: [react()],
-//   server: {
-//     proxy: {
-//       "/api/": "https://todolist-app-d0wu.onrender.com",
-//       "/uploads/": "https://todolist-app-d0wu.onrender.com",
-//     },
-//   },
-// });
-
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
 
@@ -20,8 +6,22 @@ export default defineConfig({
   plugins: [react()],
   server: {
     proxy: {
-      "/api/": "http://localhost:5000/",
-      "/uploads/": "http://localhost:5000/",
+      "/api/": "https://todolist-app-d0wu.onrender.com",
+      "/uploads/": "https://todolist-app-d0wu.onrender.com",
     },
   },
 });
+
+// import { defineConfig } from "vite";
+// import react from "@vitejs/plugin-react";
+
+// // https://vitejs.dev/config/
+// export default defineConfig({
+//   plugins: [react()],
+//   server: {
+//     proxy: {
+//       "/api/": "http://localhost:5000/",
+//       "/uploads/": "http://localhost:5000/",
+//     },
+//   },
+// });
